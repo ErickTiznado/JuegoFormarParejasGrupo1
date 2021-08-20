0b3ed82c57fc363dc98e52f90cadf15e8c2afa10
Oscar-Velasquez <88002646+Oscar-Velasquez@users.noreply.github.com>
2021-08-20 11:39:09 -0600

    Oscar puso su nombre

--- JuegoFormarParejas/Form1.cs
+++ JuegoFormarParejas/Form1.cs
@@ -10,6 +10,8 @@ using System.Windows.Forms;
 
 namespace JuegoFormarParejas
 {
+    //Oscar Manuel Lopez Velasquez
+
     public partial class Form1 : Form
     {
         Label firstClicked = null;
